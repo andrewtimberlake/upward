f3992e54b2444c8a0e2ff370ab66e4e99f1a377c
Andrew Timberlake <andrew@andrewtimberlake.com>
2024-06-03 09:15:26 +0200

    Package info for hex

--- mix.exs
+++ mix.exs
@@ -1,14 +1,44 @@
 defmodule Upward.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/andrewtimberlake/upward"
+  @version "0.0.1"
+
   def project do
     [
       app: :upward,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: @github_url,
+      docs: fn ->
+        [
+          source_ref: "#{@version}",
+          canonical: "http://hexdocs.pm/upward",
+          main: "Upward",
+          source_url: @github_url,
+          extras: ["README.md"]
+        ]
+      end,
+      description: description(),
+      package: package()
+    ]
+  end
+
+  defp description do
+    """
+    A library to assist with hot-code upgrades with Elixir releases
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Andrew Timberlake"],
+      contributors: ["Andrew Timberlake"],
+      licenses: ["MIT"],
+      links: %{"Github" => @github_url}
     ]
   end
 
@@ -28,6 +58,7 @@ defmodule Upward.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
